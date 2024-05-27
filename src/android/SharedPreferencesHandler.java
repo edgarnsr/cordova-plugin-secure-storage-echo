8c75d197fb05daddc82cf3f5236ac1be79a1bc02
Edgar Noel Salmerón Ramírez <12400976+edgarnsr@users.noreply.github.com>
2024-05-26 19:31:44 -0700

    Update SharedPreferencesHandler.java

--- src/android/SharedPreferencesHandler.java
+++ src/android/SharedPreferencesHandler.java
@@ -1,54 +1,72 @@
 package com.crypho.plugins;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Iterator;
 
 import android.content.SharedPreferences;
 import android.content.Context;
+import android.util.Log;
+
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKeys;
 
 public class SharedPreferencesHandler {
-	private SharedPreferences prefs;
+    private SharedPreferences prefs;
 
-	public SharedPreferencesHandler (String prefsName, Context ctx){
-		prefs = ctx.getSharedPreferences(prefsName  + "_SS", 0);
-	}
-
-	boolean isEmpty() {
-	    return prefs.getAll().isEmpty();
+    public SharedPreferencesHandler(String prefsName, Context ctx) {
+        try {
+            String masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
+            prefs = EncryptedSharedPreferences.create(
+                    prefsName + "_SS",
+                    masterKeyAlias,
+                    ctx,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+        } catch (GeneralSecurityException | IOException e) {
+            Log.e("SharedPreferenceHandler", "Error creating EncryptedSharedPreferences", e);
+            prefs = null;
+        }
     }
 
-    public void store(String key, String value){
+    SharedPreferences getPrefs() {
+        return prefs;
+    }
+
+    void store(String key, String value) {
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("_SS_" + key, value);
-        editor.commit();
+        editor.apply();
     }
 
-    String fetch (String key){
+    String fetch(String key) {
         return prefs.getString("_SS_" + key, null);
     }
 
-    void remove (String key){
+    void remove(String key) {
         SharedPreferences.Editor editor = prefs.edit();
         editor.remove("_SS_" + key);
-        editor.commit();
+        editor.apply();
     }
 
-    Set keys (){
-        Set res = new HashSet<String>();
-    	Iterator<String> iter = prefs.getAll().keySet().iterator();
+    Set<String> keys() {
+        Set<String> res = new HashSet<>();
+        Iterator<String> iter = prefs.getAll().keySet().iterator();
         while (iter.hasNext()) {
             String key = iter.next();
-            if (key.startsWith("_SS_")  && !key.startsWith("_SS_MIGRATED_")) {
+            if (key.startsWith("_SS_") && !key.startsWith("_SS_MIGRATED_")) {
                 res.add(key.replaceFirst("^_SS_", ""));
             }
         }
         return res;
     }
 
-    void clear (){
+    void clear() {
         SharedPreferences.Editor editor = prefs.edit();
         editor.clear();
-        editor.commit();
+        editor.apply();
     }
 }
